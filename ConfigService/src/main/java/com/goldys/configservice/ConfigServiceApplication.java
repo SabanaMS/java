8763d90f7d892ad59eb23ba76b6464d152b7f623
Somsubhra Mukherjee <somsubhramukherjee@Somsubhras-Official-MacBook-Air.local>
2020-04-08 10:42:39 +0530

    completed Config Service

--- ConfigService/src/main/java/com/goldys/configservice/ConfigServiceApplication.java
+++ ConfigService/src/main/java/com/goldys/configservice/ConfigServiceApplication.java
@@ -4,6 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/*
+Spring Cloud Config provides server and client-side support for externalized
+configuration in a distributed system. With the Config Server you have a central
+place to manage external properties for applications across all environments.
+
+Add the annotation @EnableConfigServer, which defines that this application will
+work as the externalized configuration server.
+*/
+
+
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
